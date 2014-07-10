a736d21333551fb5e3b4b5f3d4c00a049f9b3b28
Mewes Kochheim <mewes@kochheim.de>
2014-07-10 23:07:21 +0200

    Advanced validation for regex options

--- src/MewesK/WebRedirectorBundle/Validator/Constraints/IsValidPCRE.php
+++ src/MewesK/WebRedirectorBundle/Validator/Constraints/IsValidPCRE.php
@@ -9,5 +9,6 @@ use Symfony\Component\Validator\Constraint;
  */
 class IsValidPCRE extends Constraint
 {
-    public $message = 'The string "%string%" must be a valid Perl Compatible Regular Expression';
+    public $message = 'The string "%string%" must be a valid Perl Compatible Regular Expression using slashes (/) as delimiters.';
+    public $messageOptions = 'The string "%string%" must only contain the following options: i, m, s and x.';
 }
--- src/MewesK/WebRedirectorBundle/Validator/Constraints/IsValidPCREValidator.php
+++ src/MewesK/WebRedirectorBundle/Validator/Constraints/IsValidPCREValidator.php
@@ -7,12 +7,25 @@ use Symfony\Component\Validator\ConstraintValidator;
 
 class IsValidPCREValidator extends ConstraintValidator
 {
-    const REGEX_PCRE = '/^\/.*\/[imox]{0,4}$/';
+    const REGEX_PCRE = '/^\/.*\/(.*)$/';
+    const REGEX_OPTIONS = '/^[imsx]{0,4}$/';
 
     public function validate($value, Constraint $constraint)
     {
+        if (empty($value)) {
+            return;
+        }
+
         /** @var $constraint IsValidPCRE */
-        if (!empty($value) && !preg_match(self::REGEX_PCRE, $value, $matches)) {
+
+        if (preg_match(self::REGEX_PCRE, $value, $matches)) {
+            if (!preg_match(self::REGEX_OPTIONS, $matches[1], $matches)) {
+                $this->context->addViolation(
+                    $constraint->messageOptions,
+                    array('%string%' => $value)
+                );
+            }
+        } else {
             $this->context->addViolation(
                 $constraint->message,
                 array('%string%' => $value)
