3d58ce78d9748350c58c21162249e8845beddc5c
Mewes Kochheim <mewes@kochheim.de>
2014-07-10 23:02:36 +0200

    Switched to services.yml

--- src/MewesK/WebRedirectorBundle/DependencyInjection/MewesKWebRedirectorExtension.php
+++ src/MewesK/WebRedirectorBundle/DependencyInjection/MewesKWebRedirectorExtension.php
@@ -4,6 +4,7 @@ namespace MewesK\WebRedirectorBundle\DependencyInjection;
 
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 use Symfony\Component\DependencyInjection\Loader;
 
@@ -22,7 +23,7 @@ class MewesKWebRedirectorExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.xml');
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader->load('services.yml');
     }
 }
